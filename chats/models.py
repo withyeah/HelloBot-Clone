990384b73286e0dd6f7755cb7328ce28ecdbccf2
Alex Gim <yerang.dev@gmail.com>
2021-05-02 16:28:18 +0900

    Alter models.py

--- chats/models.py
+++ chats/models.py
@@ -5,10 +5,10 @@ from django.db import models
 class Scenario(models.Model):
     input_message = models.CharField(max_length=255)
     output_message = models.TextField()
-    next_question = models.CharField(max_length=255)
+    next_question = models.CharField(max_length=255, blank=True)
 
 class Tarot(models.Model):
-    card_number = models.IntegerField()
+    card_number = models.IntegerField(unique=True)
     card_image = models.ImageField()
     explanation = models.TextField()
 
